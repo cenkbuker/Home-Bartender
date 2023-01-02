4a85f21405f02240235d033160ed51946c57fb09
Cenk <bukercenk@gmail.com>
2023-01-01 20:06:18 -0600

    Styling for register and login page

--- forms.py
+++ forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Length
 
 class UserAddForm(FlaskForm):
     """Form for adding users."""
-    first_name = StringField('First name', validators=[DataRequired()])
+    first_name = StringField('First name', validators=[DataRequired()] )
     last_name = StringField('Last name', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
